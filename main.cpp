ff67022f21c47852b5a29145acf56111b08d8ac0
zhangbaojun <89057244+BaojunZhangLoveLife@users.noreply.github.com>
2022-12-27 14:55:29 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include "MainWindow.h"
 #include <QApplication>
-
-int main(int argc, char* argv[])
+#include <ItkSegmentation.h>
+#include <GraphCut.h>
+//int main(int argc, char* argv[])
+//{
+//	QApplication a(argc, argv);
+//
+//	MainWindow w;
+//	w.show();
+//	return a.exec();
+//
+//
+//}
+int main()
 {
-	QApplication a(argc, argv);
-
-	MainWindow w;
-	w.show();
-	return a.exec();
-
-
-}
+	GraphCutAlgorithm* graphcut = new GraphCutAlgorithm();
+	std::string srcString = "C:\\Users\\Baojun\\Downloads\\bone-segmentation-master\\sample-volumes\\001-CT.nii";
+	std::string fgString = "C:\\Users\\Baojun\\Downloads\\bone-segmentation-master\\sample-volumes\\FG.nii";
+	std::string bgString = "C:\\Users\\Baojun\\Downloads\\bone-segmentation-master\\sample-volumes\\BG.nii";
+	std::string dstString = "C:\\Users\\Baojun\\Downloads\\bone-segmentation-master\\sample-volumes\\DST.nii";
+	graphcut->GraphCut(srcString, fgString, bgString, dstString);
+ }
